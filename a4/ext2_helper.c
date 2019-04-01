09b386200169e6c451083e7eb9381457b8b2a29e
peizhizhang <peizhi.zhang@mail.utoronto.ca>
2019-04-01 16:05:52 -0400

    commit

---
+++ a4/ext2_helper.c
@@ -0,0 +1,92 @@
+#include "ext2.h"
+
+int allocate_block(unsigned char *disk, struct ext2_group_desc *bg, unsigned int blocks_count) {
+    unsigned char *block_bitmap = disk + bg->bg_block_bitmap * EXT2_BLOCK_SIZE;
+    int size = blocks_count / 8;
+    for(int i = 0; i < size; i++) {
+        unsigned char e = block_bitmap[i];
+        for(int j = 0; j < 8; j++) {
+            int temp = !!((e << i) & 0x01);
+            if (temp == 0) {
+                e |= 0x80 >> i;
+                return i * 8 + j;
+            }
+        }
+    }
+}
+
+int allocate_inode(unsigned char *disk, struct ext2_group_desc *bg, unsigned int inodes_count){
+    unsigned char *inode_bitmap = disk + bg->bg_inode_bitmap * 1024;
+    int inode_size = inodes_count / 8;
+    for(int i = 0; i < inode_size; i++) {
+        unsigned char e = inode_bitmap[i];
+        for(int j = 0; j < 8; j++) {
+            int temp = !!((e << i) & 0x01);
+            if (temp == 0) {
+                e |= 0X80 >> i;
+                return i * 8 + j;
+            }
+      }
+  }
+}
+
+// search within a directory for a file or directory; if successful, return the
+// inode; if not, return -1
+int search_dir(char *substring, struct ext2_inode *current) {
+    struct ext2_dir_entry *dir = (struct ext2_dir_entry *) (disk + EXT2_BLOCK_SIZE * current -> i_block[0]);
+    int rec_len_sum = 0;
+    int next_inode = -1;
+    while (rec_len_sum < EXT2_BLOCK_SIZE) {
+        dir = (struct ext2_dir_entry *) (disk + EXT2_BLOCK_SIZE * current -> i_block[0] + rec_len_sum);
+        unsigned int inode = dir -> inode;
+        unsigned short rec_len = dir -> rec_len;
+        char *name = dir -> name;
+        if (strcmp(name, substring) == 0) {
+            next_inode = inode;
+            break;
+        }
+        rec_len_sum += rec_len;
+    }
+    if (next_inode == -1) {
+        return -1;
+    }
+    return next_inode;
+}
+
+// Get the name of last entry in path, and the parent inode
+int get_last_name(struct ext2_inode *inode_table, struct ext2_inode *root, char *path, struct ext2_inode *parent_inode, char *name) {
+     char *start = path + 1;
+     char *end = strchr(start, '/');
+     char *substring = malloc(sizeof(char) * 1024);
+     if (end == NULL) {
+         name = start;
+         name[strlen(start)] = '\0';
+         result[0] = *root;
+     } else {
+         strncpy(substring, start, end - start);
+         substring[end - start] = '\0';
+     }
+     struct ext2_inode *current = root;
+     while (end != NULL) {
+         int inode = search_dir(substring, current);
+         if (inode == -1) {
+             return ENOENT;
+         }
+
+         start = end;
+         end = strchr(start + 1, '/');
+         if (end == NULL) {
+             name = start;
+             name[strlen(start)] = '\0';
+             result[0] = *current;
+             break;
+         } else {
+             memset(substring, 0, 1024);
+             strncpy(substring, start + 1, end - start);
+             substring[end - start] = '\0';
+         }
+
+         current = inode_table + (inode - 1);
+      }
+      return 0;
+}
---
+++ a4/ext2_mkdir.c
@@ -0,0 +1,109 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "ext2.h"
+
+unsigned char *disk;
+
+int main(int argc, char **argv) {
+    if(argc != 3) {
+        fprintf(stderr, "Usage: %s <image file name>\n", argv[0]);
+        exit(1);
+    }
+
+    int fd = open(argv[1], O_RDWR);
+	  if(fd == -1) {
+		   perror("open");
+		   exit(1);
+    }
+
+    disk = mmap(NULL, 128 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(disk == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
+    struct ext2_super_block *sb = (struct ext2_super_block *)(disk + 1024);
+    struct ext2_group_desc *bg = (struct ext2_group_desc *) (disk + 2048);
+
+    unsigned int blocks_count = sb->s_blocks_count;
+    int block_num = allocate_block(disk, bg, blocks_count)
+
+    unsigned int inodes_count = sb->s_inodes_count;
+    int inode_num = allocate_inode(disk, bg, inodes_count);
+
+    // root node
+    struct ext2_inode *inode_table = (struct ext2_inode *) (disk + 1024 * bg->bg_inode_table);
+    struct ext2_inode *root = inode_table + (EXT2_ROOT_INO - 1);
+
+    // zero out the trailing slash, if there is one.
+    char *path = argv[2];
+    int length = strlen(path) - 1;
+    if (path[strlen(path) - 1] == '/') {
+        path[length] = '\0';
+    }
+
+    // traverse file path, see if there is discontinuities
+    struct ext2_inode *parent = malloc(sizeof(struct ext2_inode));
+    char name[1024];
+    int discontinuities = get_last_name(inode_table, root, path, parent, name);
+    if (discontinuities == 0) {
+        if (search_dir(name, parent) != -1) {
+            return EEXIST;
+        }
+    } else {
+      return ENOENT;
+    }
+
+    // modify parent block to make room for the new one
+    struct ext2_dir_entry *dir = (struct ext2_dir_entry *) (disk + EXT2_BLOCK_SIZE * parent.i_block[0]);
+    int rec_len_sum = 0;
+    while (rec_len_sum < EXT2_BLOCK_SIZE) {
+        dir = (struct ext2_dir_entry *) (disk + EXT2_BLOCK_SIZE * parent.i_block[0] + rec_len_sum);
+        unsigned short rec_len = dir -> rec_len;
+        if (rec_len_sum + rec_len == EXT2_BLOCK_SIZE) {
+            int raw_length = 4 + 2 + 1 + 1 + dir -> name_len;
+            int padded_length = raw_length + (4 - raw_length % 4);
+            dir -> rec_len = padded_length;
+            break;
+        }
+        rec_len_sum += rec_len;
+    }
+
+    dir = (struct ext2_dir_entry *) (disk + EXT2_BLOCK_SIZE * parent.i_block[0] + rec_len_sum);
+    dir -> inode = inode;
+    dir -> rec_len = EXT2_BLOCK_SIZE - rec_len_sum;
+    dir -> name_len = strlen(name);
+    dir -> file_type = EXT2_FT_DIR;
+    strncpy(dir -> name, name, dir -> name_len);
+
+    struct ext2_inode new = inode_table[inode];
+    new.i_mode = EXT2_S_IFDIR;
+    new.i_size = 1024;
+    new.i_blocks = 2;
+    new.i_block[0] = block;
+    new.i_links_count = 0;
+
+    struct ext2_dir_entry *self = (struct ext2_dir_entry *) (disk + EXT2_BLOCK_SIZE * block);
+    self -> inode = inode;
+    self -> rec_len = 12;
+    self -> name_len = 1;
+    self -> file_type = EXT2_FT_DIR;
+    self -> name[0] = '.';
+
+    struct ext2_dir_entry *parent_pointer = (struct ext2_dir_entry *) (disk + EXT2_BLOCK_SIZE * block + 12);
+    parent_pointer -> inode = inode;
+    parent_pointer -> rec_len = 12;
+    parent_pointer -> name_len = 2;
+    parent_pointer -> file_type = EXT2_FT_DIR;
+    parent_pointer -> name[0] = '.';
+    parent_pointer -> name[1] = '.';
+
+
+}
